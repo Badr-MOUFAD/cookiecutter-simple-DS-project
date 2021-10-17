4baf7d8df742275948ab15a29828434d0b2dc159
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2021-10-17 22:50:16 +0100

    add example py_scripts

---
+++ {{ cookiecutter.project_name }}/py_scripts/test_module.py
@@ -0,0 +1,18 @@
+
+def say_hi(name: str) -> None:
+    """Function to greet
+
+    Parameters
+    ----------
+    name : str
+        Your name
+
+    Examples
+    --------
+    >>> from py_scripts.test_module import say_hi
+    >>> say_hi("Badr MOUFAD")
+    Hi Badr MOUFAD!
+    """
+
+    print(f"Hi {name}!")
+    return
